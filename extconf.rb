c19e85333140d745f76886a17f3158ac8acd2a53
Antoine Niek <antoineniek@gmail.com>
2012-02-23 15:05:56 +0100

    update include dir

--- extconf.rb
+++ extconf.rb
@@ -2,7 +2,7 @@ require "mkmf"
 
 dir_config('frontbase')
 
-$CPPFLAGS = "-I/usr/local/FrontBase/include"
+$CPPFLAGS = "-I/usr/local/FrontBase/include/FBCAccess"
 $LDFLAGS = "-L/usr/local/FrontBase/lib"
 $libs = " -lFBCAccess "
 create_makefile("frontbase")
